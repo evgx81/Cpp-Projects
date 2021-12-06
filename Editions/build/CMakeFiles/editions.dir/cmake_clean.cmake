c331cef8ccba2149757c08a84033c48f4257877f
evgx81 <xoroshix2002@mail.ru>
2021-12-06 21:27:20 +0300

    initial commit

---
+++ Editions/build/CMakeFiles/editions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/editions.dir/editions.cpp.o"
+  "CMakeFiles/editions.dir/editions.cpp.o.d"
+  "editions"
+  "editions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/editions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
