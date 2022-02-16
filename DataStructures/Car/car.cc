24979acf02abc79c95898bd46ba228821a66c442
evgx81 <xoroshix2002@mail.ru>
2022-02-16 21:11:15 +0300

    Added car.cc

---
+++ DataStructures/Car/car.cc
@@ -0,0 +1,30 @@
+#include "car.hh"
+
+Car::Car(const Car& car)
+{
+    m_state_number = car.m_state_number;
+    m_firm_name = car.m_firm_name;
+    m_driver_name = car.m_driver_name;
+    m_route_number = car.m_route_number;
+    m_condition = car.m_condition;
+}
+
+std::ostream& operator<<(std::ostream& os, const Car& car)
+{
+    os << car.m_state_number << ' ' << car.m_firm_name << ' ' << car.m_driver_name << ' ' << car.m_route_number << car.m_condition;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Car& car)
+{
+    is >> car.m_state_number;
+    getline(is, car.m_firm_name, ' ');
+    getline(is, car.m_driver_name, ' ');
+
+    // Правильной обработки считания перечисления
+    int condition{};
+    is >> car.m_route_number >> condition;
+    car.m_condition = condition == 0 ? PARK : ROUTE;
+    is.ignore();
+    return is;
+}
