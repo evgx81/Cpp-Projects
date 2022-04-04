0470ad17f4ac839376e95b6ea8addfd3c8ec7fa6
evgx81 <xoroshix2002@mail.ru>
2022-04-04 20:12:34 +0300

    cell tests

--- DataStructures/Balda/tests.cc
+++ DataStructures/Balda/tests.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "player.hh"
+#include "balda.hh"
 
 TEST(PlayerTest, createEmptyPlayer)
 {
@@ -24,4 +25,46 @@ TEST(PlayerTest, createFilledPlayer)
     ss << player;
     std::string player_str = ss.str();
     ASSERT_EQ(player_str, "11coolbanana"); 
+}
+
+TEST(BaldaTest, createGame)
+{
+    std::list<std::string> words = {"addd", "asss"};
+    std::string word = "adddd";
+    Balda balda = Balda{word, words};
+    std::stringstream ss;
+    ss << balda;
+    std::string balda_str = ss.str();
+    ASSERT_EQ(balda_str, "adddasss0000000000adddd0000000000"); 
+}
+
+TEST(CellTest, createEmptyCell)
+{
+    Cell cell = Cell{};
+    std::stringstream ss;
+    ss << cell;
+    std::string cell_str = ss.str();
+    ASSERT_EQ(cell_str, "00"); 
+}
+
+TEST(CellTest, readCell)
+{
+    Cell cell = Cell();
+    std::string data = "1 1";
+    std::stringstream ss(data);
+    ss >> cell;
+    std::stringstream ss1;
+    ss1 << cell;
+    std::string s = ss1.str();
+    EXPECT_EQ(s, "11");
+}
+
+TEST(CellTest, copyCell)
+{
+    Cell cell1 = Cell(3, 2);
+    Cell cell2 = cell1;
+    std::stringstream ss1;
+    ss1 << cell2;
+    std::string s = ss1.str();
+    EXPECT_EQ(s, "32");
 }
